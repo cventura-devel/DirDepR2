9bed8be8654ae6096066d1a9237effb1e7098faa
Carlos Ventura <cventura@outlook.com>
2015-05-13 00:00:44 -0600

    se añade control de admin para profesores

---
+++ DirDepR2.WebUI/Controllers/AdminProfesorController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DirDepR2.Domain.Abstract;
+
+namespace DirDepR2.WebUI.Controllers
+{
+    public class AdminProfesorController : Controller
+    {
+        private IProfesorRepository repository;
+
+        public AdminProfesorController(IProfesorRepository repo)
+        {
+            repository = repo;
+        }
+
+        public ActionResult Index()
+        {
+            return View(repository.Profesors);
+        }
+    }
+}
